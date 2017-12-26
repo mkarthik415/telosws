059d71bde28acf326099464e17130e239c3e279a
Karthik Marupeddi <mkarthik415@gmail.com>
2017-12-26 09:26:39 -0600

    fixed tomcat issues

--- src/main/java/telosws/TeloswsApplication.java
+++ src/main/java/telosws/TeloswsApplication.java
@@ -5,10 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
